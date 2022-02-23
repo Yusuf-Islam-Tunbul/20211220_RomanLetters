a764aacdfc4c4963651b69f7da7e06a795fe3344
Yusuf-Islam-Tunbul <50180280+Yusuf-Islam-Tunbul@users.noreply.github.com>
2022-02-23 21:33:03 +0300

    Proje dosyası ekle.

---
+++ 20211220_RomanLetters/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace _20211220_RomanLetters
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_textbox = new System.Windows.Forms.TextBox();
+            this.convert_button = new System.Windows.Forms.Button();
+            this.output_label = new System.Windows.Forms.Label();
+            this.input_textbox2 = new System.Windows.Forms.TextBox();
+            this.deconvert_button = new System.Windows.Forms.Button();
+            this.output_label2 = new System.Windows.Forms.Label();
+            this.info_label1 = new System.Windows.Forms.Label();
+            this.info_label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input_textbox
+            // 
+            this.input_textbox.Location = new System.Drawing.Point(12, 35);
+            this.input_textbox.Name = "input_textbox";
+            this.input_textbox.Size = new System.Drawing.Size(136, 22);
+            this.input_textbox.TabIndex = 0;
+            // 
+            // convert_button
+            // 
+            this.convert_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.convert_button.Location = new System.Drawing.Point(12, 63);
+            this.convert_button.Name = "convert_button";
+            this.convert_button.Size = new System.Drawing.Size(136, 23);
+            this.convert_button.TabIndex = 1;
+            this.convert_button.Text = "ÇEVİR";
+            this.convert_button.UseVisualStyleBackColor = true;
+            this.convert_button.Click += new System.EventHandler(this.convert_button_Click);
+            // 
+            // output_label
+            // 
+            this.output_label.AutoSize = true;
+            this.output_label.Location = new System.Drawing.Point(12, 89);
+            this.output_label.Name = "output_label";
+            this.output_label.Size = new System.Drawing.Size(0, 17);
+            this.output_label.TabIndex = 2;
+            // 
+            // input_textbox2
+            // 
+            this.input_textbox2.Location = new System.Drawing.Point(162, 35);
+            this.input_textbox2.Name = "input_textbox2";
+            this.input_textbox2.Size = new System.Drawing.Size(136, 22);
+            this.input_textbox2.TabIndex = 3;
+            // 
+            // deconvert_button
+            // 
+            this.deconvert_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.deconvert_button.Location = new System.Drawing.Point(162, 63);
+            this.deconvert_button.Name = "deconvert_button";
+            this.deconvert_button.Size = new System.Drawing.Size(136, 23);
+            this.deconvert_button.TabIndex = 4;
+            this.deconvert_button.Text = "ÇEVİR";
+            this.deconvert_button.UseVisualStyleBackColor = true;
+            this.deconvert_button.Click += new System.EventHandler(this.deconvert_button_Click_1);
+            // 
+            // output_label2
+            // 
+            this.output_label2.AutoSize = true;
+            this.output_label2.Location = new System.Drawing.Point(159, 89);
+            this.output_label2.Name = "output_label2";
+            this.output_label2.Size = new System.Drawing.Size(0, 17);
+            this.output_label2.TabIndex = 5;
+            // 
+            // info_label1
+            // 
+            this.info_label1.AutoSize = true;
+            this.info_label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.info_label1.Location = new System.Drawing.Point(12, 10);
+            this.info_label1.Name = "info_label1";
+            this.info_label1.Size = new System.Drawing.Size(127, 20);
+            this.info_label1.TabIndex = 6;
+            this.info_label1.Text = "Tam sayı giriniz";
+            // 
+            // info_label2
+            // 
+            this.info_label2.AutoSize = true;
+            this.info_label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.info_label2.Location = new System.Drawing.Point(158, 10);
+            this.info_label2.Name = "info_label2";
+            this.info_label2.Size = new System.Drawing.Size(158, 20);
+            this.info_label2.TabIndex = 7;
+            this.info_label2.Text = "Roma rakamı giriniz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 123);
+            this.Controls.Add(this.info_label2);
+            this.Controls.Add(this.info_label1);
+            this.Controls.Add(this.output_label2);
+            this.Controls.Add(this.deconvert_button);
+            this.Controls.Add(this.input_textbox2);
+            this.Controls.Add(this.output_label);
+            this.Controls.Add(this.convert_button);
+            this.Controls.Add(this.input_textbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_textbox;
+        private System.Windows.Forms.Button convert_button;
+        private System.Windows.Forms.Label output_label;
+        private System.Windows.Forms.TextBox input_textbox2;
+        private System.Windows.Forms.Button deconvert_button;
+        private System.Windows.Forms.Label output_label2;
+        private System.Windows.Forms.Label info_label1;
+        private System.Windows.Forms.Label info_label2;
+    }
+}
+
---
+++ 20211220_RomanLetters/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211220_RomanLetters
+{
+    public partial class Form1 : Form
+    {
+        List<string> letters = new List<string> {"I","V","X","L","C","D","M"};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convert_button_Click(object sender, EventArgs e)
+        {
+            ConvertIfValid();
+        }
+
+        void ConvertIfValid()
+        {
+            int? number = GetNumber();
+
+            if (number != null)
+            {
+                output_label.Text= ConvertMethod((int)number);
+            }
+
+            else
+            {
+                output_label.Text = "";
+            }
+        }
+
+        int? GetNumber()
+        {
+            int? number = null;
+            int temp_number = 0;
+
+            try
+            {
+                temp_number = Convert.ToInt32(input_textbox.Text);
+
+                if (temp_number > 3999)
+                {
+                    MessageBox.Show("Lütfen 3999'dan küçük bir sayı giriniz.");
+                }
+
+                else if (temp_number <= 0)
+                {
+                    MessageBox.Show("Lütfen 0'dan büyük bir sayı giriniz.");
+                }
+
+                else
+                {
+                    number = temp_number;
+                    //output_label.Text = ConvertMethod(number);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lütfen bir tam sayı giriniz.");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Lütfen 3999'dan küçük bir sayı giriniz.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return number;
+        }
+
+        string ConvertMethod(int number)
+        {
+            string text = "";
+
+            for (int i = 3; i >= 0; i--)
+            {
+                int division = number / Convert.ToInt32(Math.Pow(10, i));
+
+                number = number % Convert.ToInt32(Math.Pow(10, i));
+
+                switch (division)
+                {
+                    case 1:
+                        text += letters[i * 2];
+                        break;
+                    case 2:
+                        text += letters[i * 2] + letters[i * 2];
+                        break;
+                    case 3:
+                        text += letters[i * 2] + letters[i * 2] + letters[i * 2];
+                        break;
+                    case 4:
+                        text += letters[i * 2] + letters[i * 2 + 1];
+                        break;
+                    case 5:
+                        text += letters[i * 2 + 1];
+                        break;
+                    case 6:
+                        text += letters[i * 2 + 1] + letters[i * 2];
+                        break;
+                    case 7:
+                        text += letters[i * 2 + 1] + letters[i * 2] + letters[i * 2];
+                        break;
+                    case 8:
+                        text += letters[i * 2 + 1] + letters[i * 2] + letters[i * 2] + letters[i * 2];
+                        break;
+                    case 9:
+                        text += letters[i * 2] + letters[i * 2 + 2];
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return text;
+        } 
+
+        private void deconvert_button_Click_1(object sender, EventArgs e)
+        {
+            DeconvertIfValid();
+        }
+
+        void DeconvertIfValid()
+        {
+            string input_text = input_textbox2.Text.ToUpper();
+
+            input_text=input_text.Replace("İ", "I");
+
+            output_label2.Text = (DeconvertMethod(input_text)).ToString();
+
+            if (output_label2.Text == "0")
+            {
+                MessageBox.Show("Lütfen geçerli bir metin girişi yapınız.");
+            }
+        }
+
+        int DeconvertMethod(string text)
+        {
+            text += "ööö";
+            int number = 0;
+
+            while (text[0] == 'M')
+            {
+                number += 1000;
+                text = text.Remove(0, 1);
+            }
+
+            for (int i = 2; i >= 0; i--)
+            {
+                if (text[0].ToString() == letters[2 * i + 1])
+                {
+                    if (text[1].ToString() == letters[2 * i])
+                    {
+                        if (text[2].ToString() == letters[2 * i])
+                        {
+                            if (text[3].ToString() == letters[2 * i])
+                            {
+                                number += 8 * Convert.ToInt32(Math.Pow(10, i));
+                                text = text.Remove(0, 4);
+                            }
+
+                            else
+                            {
+                                number += 7 * Convert.ToInt32(Math.Pow(10, i));
+                                text = text.Remove(0, 3);
+                            }
+                        }
+
+                        else
+                        {
+                            number += 6 * Convert.ToInt32(Math.Pow(10, i));
+                            text = text.Remove(0, 2);
+                        }
+                    }
+
+                    else
+                    {
+                        number += 5 * Convert.ToInt32(Math.Pow(10, i));
+                        text = text.Remove(0, 1);
+                    }
+                }
+
+                else if (text[0].ToString() == letters[2 * i])
+                {
+                    if (text[1].ToString() == letters[2 * i + 1])
+                    {
+                        number += 4 * Convert.ToInt32(Math.Pow(10, i));
+                        text = text.Remove(0, 2);
+                    }
+
+                    else if (text[1].ToString() == letters[2 * i])
+                    {
+                        if (text[2].ToString() == letters[2 * i])
+                        {
+                            number += 3 * Convert.ToInt32(Math.Pow(10, i));
+                            text = text.Remove(0, 3);
+                        }
+
+                        else
+                        {
+                            number += 2 * Convert.ToInt32(Math.Pow(10, i));
+                            text = text.Remove(0, 2);
+                        }
+                    }
+
+                    else
+                    {
+                        number += Convert.ToInt32(Math.Pow(10, i));
+                        text = text.Remove(0, 1);
+                    }
+                }
+            }
+
+            text = text.Trim('ö');
+
+            return (text.Length == 0 ? number : 0);
+        }
+    }    
+}
